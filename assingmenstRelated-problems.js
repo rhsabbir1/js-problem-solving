dc3a7ba55c92bdc0b8ef4d89477909940b548056
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-04 17:32:52 +0600

    Cheek object match or not

--- assingmenstRelated-problems.js
+++ assingmenstRelated-problems.js
@@ -24,15 +24,37 @@ const deg = radianToDegree(199)
 
 // ফাংশন নেম দিতে হবে isJavaScriptFile । এই ফাংশনে প্যারামিটার হিসেবে নিবে একটি স্ট্রিং(String) যেটি হবে একটি ফাইল নেম (যেমনঃ ‘index.js’)। যদি এটি জাভাস্ক্রিপ্ট ফাইল হয় তোমাকে true রিটার্ন করতে হবে আর যদি না হয় তাহলে false রিটার্ন করতে হবে ।
 
-function isJavaScriptFile(fileName){
-    if(fileName.endsWith('.js')){
+function isJavaScriptFile(fileName) {
+    if (fileName.endsWith('.js')) {
         return true;
     }
-    else{
+    else {
         return false;
     }
-   
+
 }
 
 const file = isJavaScriptFile('apps.js')
-console.log(file)
+// console.log(file)
+
+
+// Problem 5:  isBestFriend
+
+
+// তোমাকে একটি ফাংশন লিখতে হবে isBestFriend যেটি  প্যরামিটার হিসেবে দুইটি অবজেক্ট নিবে । তোমাকে অবজেক্ট দুইটি নিয়ে দেখতে হবে এরা দুইজন একে অপরের বেস্টফ্রেন্ড কিনা । যদি  বেস্ট ফ্রেন্ড হয় তাহলে true রিটার্ন করবে , না হলে false রিটার্ন করবে ।
+
+
+function isBestFriend(objectOne, objectTow) {
+    if (objectOne.name == objectTow.friend && objectTow.name == objectOne.friend){
+        console.log('True')
+    }
+    else{
+        console.log('False')
+    }
+}
+
+const bestFriend = isBestFriend(
+    { name: "abul", friend: "babul" },
+
+    { name: "babul", friend: "abul" }
+)
