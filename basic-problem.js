535072e9e2d159188caa832423ebf363836ec3d7
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-04 14:47:20 +0600

    basic problem

---
+++ basic-problem.js
@@ -0,0 +1,30 @@
+// 1 to n summition
+let n = 10;
+let sum = 0;
+
+for(let i = 0; i <= n; i++){
+    sum = sum + i;
+}
+// console.log(sum)
+
+// TimesTable  3
+
+for(let i = 1; i<=10; i++){
+    let timesTable = i * 3;
+    // console.log(i + ' X ' + 3 +' = '+ timesTable)
+}
+
+let arr = [-1, 2,-3,4,5,6,-7,8,-9,10]
+function gattrThenFive(numbers){
+    let count = 0;
+    for(let i =0; i < numbers.length; i++){
+        const number = numbers[i];
+        if(number > 5){
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+const count = gattrThenFive(arr)
+console.log(count)
